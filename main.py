2dbf26fd02dde5008dafa94c921e549fc9803075
patpietryg <patpietryg@gmail.com>
2024-04-13 14:12:36 +0200

    First commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import sys
+from PySide6.QtWidgets import QApplication
+from app import App
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = App()
+    window.show()
+    sys.exit(app.exec())
